adbd6a64f5e73830853810fa678d05de4904742a
Madhumitha1621 <157510768+Madhumitha1621@users.noreply.github.com>
2024-09-21 00:14:09 +0530

    Add files via upload

---
+++ owner/includes/leftbar.php
@@ -0,0 +1,41 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Main</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+			
+<li><a href="#"><i class="fa fa-files-o"></i> Brands</a>
+<ul>
+<li><a href="create-brand.php">Create Brand</a></li>
+<li><a href="manage-brands.php">Manage Brands</a></li>
+</ul>
+</li>
+
+<li><a href="#"><i class="fa fa-sitemap"></i> Vehicles</a>
+					<ul>
+						<li><a href="post-avehical.php">Post a Vehicle</a></li>
+						<li><a href="manage-vehicles.php">Manage Vehicles</a></li>
+					</ul>
+				</li>
+				<li><a href="manage-bookings.php"><i class="fa fa-users"></i> Manage Booking</a></li>
+
+				<li><a href="testimonials.php"><i class="fa fa-table"></i> Manage Feedback</a></li>
+			
+				
+			
+			<li><a href="update-contactinfo.php"><i class="fa fa-files-o"></i> Update Contact Info</a></li>
+			<li><a href="add_staff.php"><i class="fa fa-files-o"></i> Add Staff</a></li>
+
+			<li><a href="manage-subscribers.php"><i class="fa fa-table"></i> Manage Subscribers</a></li>
+			
+			<li><a href="#"><i class="fa fa-sitemap"></i> Report</a>
+			<ul>
+					<li><a href="reg-users.php"><i class="fa fa-users"></i> Customer Wise Report</a></li>
+						<li><a href="booking.php"><i class="fa fa-booking"></i> Booking Wise Report</a></li>
+						<li><a href="bike.php"><i class="fa fa-motorcycle"></i> Bike Wise Report</a></li>
+					
+					</ul>
+                      </li>
+
+			</ul>
+		</nav>
